96df109d8d75eb151a2b8da870aed842874f4766
hnamdev21 <hnam.tran0421@gmail.com>
2023-02-04 13:53:12 +0700

    update

--- src/angularjs/app/homeFestival/home-festival.component.js
+++ src/angularjs/app/homeFestival/home-festival.component.js
@@ -4,15 +4,15 @@ angular.module("homeFestival")
     .component("homeFestival", {
         templateUrl: "src/angularjs/templates/home-festival.html",
         controller: function(Festivals, $scope) {
-            Festivals.query(function(data) {
-                $scope.homeFes = []
+            $scope.homeFes = []
 
+            Festivals.query(function(data) {
                 $scope.festivals = data
                 pushFes(2, $scope.festivals)
             })
 
             function pushFes(num) {
-                for(let i = 0; i < 2; i++) {
+                for(let i = 0; i < num; i++) {
                     $scope.homeFes.push($scope.festivals[i])
                 }
             }
