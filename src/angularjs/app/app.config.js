0d8bf38448b9c7bd1052b058a020abcc7846e940
hnamdev21 <hnam.tran0421@gmail.com>
2023-02-06 05:55:43 +0700

    update

--- src/angularjs/app/app.config.js
+++ src/angularjs/app/app.config.js
@@ -1,29 +1,28 @@
-'use strict'
+"use strict";
 
-angular.module("app")
-    .config(function($routeProvider, $locationProvider) {
-        $locationProvider.html5Mode({
-            enabled: true,
-            // requireBase: false
-        })
+angular.module("app").config(function ($routeProvider, $locationProvider) {
+  $locationProvider.html5Mode({
+    enabled: true,
+    // requireBase: false
+  });
 
-        $routeProvider
-            .when("/", {
-                template: "<home-page></home-page>" 
-            })
-            .when("/contact", {
-                template: "<contact-page></contact-page>"
-            })
-            .when("/about", {
-                template: "<about-page></about-page>"
-            })
-            .when("/gallery", {
-                template: "<gallery-page></gallery-page>"
-            })
-            .when("/gallery/beach/:id", {
-                template: "<beach-detail></beach-detail>"
-            })
-            .when("/festivals", {
-                template: "<festival-page></festival-page>"
-            })
+  $routeProvider
+    .when("/", {
+      template: "<home-page></home-page>",
     })
+    .when("/beauty-of-beaches-demo/contact", {
+      template: "<contact-page></contact-page>",
+    })
+    .when("/beauty-of-beaches-demo/about", {
+      template: "<about-page></about-page>",
+    })
+    .when("/beauty-of-beaches-demo/gallery", {
+      template: "<gallery-page></gallery-page>",
+    })
+    .when("/beauty-of-beaches-demo/gallery/beach/:id", {
+      template: "<beach-detail></beach-detail>",
+    })
+    .when("/beauty-of-beaches-demo/festivals", {
+      template: "<festival-page></festival-page>",
+    });
+});
